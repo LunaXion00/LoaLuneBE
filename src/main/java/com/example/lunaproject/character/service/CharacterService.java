b9cf3c3c96f17ed5856e3cb038eaa9d2c3e38616
LunaXion00 <77385822+LunaXion00@users.noreply.github.com>
2025-01-10 16:14:36 +0900

    api key 제거

--- src/main/java/com/example/lunaproject/character/service/CharacterService.java
+++ src/main/java/com/example/lunaproject/character/service/CharacterService.java
@@ -38,7 +38,7 @@ public class CharacterService {
             httpURLConnection.setRequestMethod("GET");
             httpURLConnection.setRequestProperty("accept", "application/json");
             httpURLConnection.setRequestProperty("authorization",
-                    "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDA1NzExOTQifQ.Qr8rpHlBX6TnjHo5hbEgWSWZrkKj3LT4D1Z36c1gSULWuA-8ZqCgGa4TsoWgFf8k3b6-8Sv90kCpWZpPyI13kVREYrd_oLdq6zMxfsqXxfs8vE7yqTSF50PCEye8ToeH1OVMTSNAgPhmtsUzH0SHo5haMlwry5McMJBSH_ozCxVpnEf1PGLnnUMUJgTXFX24FgHMpge0d1TG4btsTlTneobKuaMYPgQeLsl4Q-orRkXlYx35VL1aBnGjTuVSOrfyuIQijl3eLODB-qDaO-tgvSkg4g9PMfAi9pvp-16oA8eEY_w7zxF8DuLRfkeXKAUx1vPX_9cMo4Te48vUgtRKQg");
+                    "bearer API-KEY");
             int responseCode = httpURLConnection.getResponseCode();
             InputStream inputStream;
             if(responseCode == 200){
@@ -63,7 +63,7 @@ public class CharacterService {
             URL url = new URL("https://developer-lostark.game.onstove.com/armories/characters/"+characterName+"/profiles");
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             httpURLConnection.setRequestMethod("GET");
-            httpURLConnection.setRequestProperty("authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDA1NzExOTQifQ.Qr8rpHlBX6TnjHo5hbEgWSWZrkKj3LT4D1Z36c1gSULWuA-8ZqCgGa4TsoWgFf8k3b6-8Sv90kCpWZpPyI13kVREYrd_oLdq6zMxfsqXxfs8vE7yqTSF50PCEye8ToeH1OVMTSNAgPhmtsUzH0SHo5haMlwry5McMJBSH_ozCxVpnEf1PGLnnUMUJgTXFX24FgHMpge0d1TG4btsTlTneobKuaMYPgQeLsl4Q-orRkXlYx35VL1aBnGjTuVSOrfyuIQijl3eLODB-qDaO-tgvSkg4g9PMfAi9pvp-16oA8eEY_w7zxF8DuLRfkeXKAUx1vPX_9cMo4Te48vUgtRKQg");
+            httpURLConnection.setRequestProperty("authorization", "Bearer API-KEY");
             httpURLConnection.setRequestProperty("accept","application/json");
             httpURLConnection.setRequestProperty("content-Type","application/json");
             httpURLConnection.setDoOutput(true);
