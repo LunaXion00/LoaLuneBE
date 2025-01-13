27bf279705b566e0d0ed572353e8f69e2f9324e0
김동하 <lunaof12@gmail.com>
2025-01-13 14:55:42 +0900

    git 변경 테스트

--- src/main/java/com/example/lunaproject/streamer/service/StreamerService.java
+++ src/main/java/com/example/lunaproject/streamer/service/StreamerService.java
@@ -1,4 +1,12 @@
 package com.example.lunaproject.streamer.service;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class StreamerService {
+
 }
